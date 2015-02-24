10bb86c2322e66482dc8df874ec71f0a01cb9bb5
Mario Iván García Flores <vanoushka3011@gmail.com>
2015-02-24 16:30:05 -0600

    Stylesheets added
    All basic styles ready to use, login and signup form already using them

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
   resources :users
 end
