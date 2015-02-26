68bb148b1de536ba690c3501d34fd596f562cf17
Juan Zenon <juanjoze0424@gmail.com>
2015-02-26 01:15:14 -0600

    Now you can login using either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  attr_accessor :login
+
   def set_default_role
     self.role ||= :user
   end
@@ -23,11 +25,22 @@ class User < ActiveRecord::Base
 
   # Not necesary, because email regexp is set in config/initializers/devise.rb
   #validates :email, format: {with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/}, uniqueness: true
-  
+
   validates :name, presence: true, format: {with: /\A[A-ZÁÉÍÓÚÑ\s]{1}[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{2,49}\z/}
   validates :last_name, presence: true, format: {with: /\A[A-ZÁÉÍÓÚÑ\s]{1}[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{2,49}\z/}
 
-  validates :username, presence: true, uniqueness: true,
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
             length: { in: 5..20, too_short: "Tiene que tener al menos 5 caracteres", too_long: "Debe tener máximo 20 caracteres" },
             format: { with: /([A-Za-z0-9\-\_\.]+)/, message: "Username sólo puede contener letras, números, guiones y puntos" }
+
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
+
 end
